c99397a9d3c9a49d77fd285dbc93168abcaae94f
Daniel <roghoh@gmail.com>
2023-09-27 03:34:53 +0400

    created elements_test / TextBox updated: data.py , elements_page.py , elements_test.py , generator.py

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -10,7 +10,10 @@ class TestElements:
             url = "https://demoqa.com/text-box"
             text_box_page = TextBoxPage(driver, url)
             text_box_page.open()
-            text_box_page.fill_all_fields()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             time.sleep(2)
             output_name, output_email, output_cur_addr, output_perm_addr = text_box_page.check_filled_form()
-            print(output_name, output_email, output_cur_addr, output_perm_addr )
+            assert full_name == output_name, "full name does not match to output name"
+            assert email == output_email, "email does not match to output email"
+            assert current_address == output_cur_addr, "current address does not match to output_current address"
+            assert permanent_address == output_perm_addr, "permanent address does not match to output permanent address"
