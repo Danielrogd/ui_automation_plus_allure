916ca23f0c4db20200fe30e44cbf951254208fa5
Daniel <roghoh@gmail.com>
2024-01-31 08:37:37 +0400

    update interactions tests
    Add class:
    - TestSelectablePage
    
    Add tests:
    - test_selectable_page

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -6,3 +6,12 @@ class SortablePageLocators:
     LIST_ITEM = (By.XPATH, "//div[@class='vertical-list-container mt-4']/div")
     TAB_GRID = (By.XPATH, "//a[@id='demo-tab-grid']")
     GRID_ITEM = (By.XPATH, "//div[@class='grid-container mt-4']//div[@class='list-group-item list-group-item-action']")
+
+
+class SelectablePageLocators:
+    TAB_LIST = (By.XPATH, "//a[@id='demo-tab-list']")
+    TAB_GRID = (By.XPATH, "//a[@id='demo-tab-grid']")
+    TAB_LIST_ITEM = (By.XPATH, "//li[@class='mt-2 list-group-item list-group-item-action']")
+    TAB_LIST_ITEM_ACTIVE = (By.XPATH, "//li[@class='mt-2 list-group-item active list-group-item-action']")
+    TAB_GRID_ITEM = (By.XPATH, "//li[@class='list-group-item list-group-item-action']")
+    TAB_GRID_ITEM_ACTIVE = (By.XPATH, "//li[@class='list-group-item active list-group-item-action']")
--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,8 +1,9 @@
-from pages.interactions_page import SortablePage
+import time
+
+from pages.interactions_page import SortablePage, SelectablePage
 
 
 class TestInteractions:
-
     class TestSortablePage:
 
         def test_sortable_page(self, driver):
@@ -13,3 +14,14 @@ class TestInteractions:
             grid_before, grid_after = sortable_page.change_grid_order()
             assert list_before != list_after, 'List items order did not changed'
             assert grid_before != grid_after, 'Grid items order did not changed'
+
+    class TestSelectablePage:
+
+        def test_selectable_page(self, driver):
+            url = "https://demoqa.com/selectable"
+            selectable_page = SelectablePage(driver, url)
+            selectable_page.open()
+            list_item = selectable_page.select_list_item()
+            grid_item = selectable_page.select_grid_item()
+            assert len(list_item) > 0, 'List item did not selected'
+            assert len(grid_item) > 0, 'Grid item did not selected'
