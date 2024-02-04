b163c390e94ef8da99efef68251867bf2c59c2ff
Daniel <roghoh@gmail.com>
2024-02-04 08:35:57 +0400

    update interactions tests
    -Base Page

--- pages/base_page.py
+++ pages/base_page.py
@@ -38,12 +38,4 @@ class BasePage:
         for item in checkbox:
             assert item in output, f" {item} checkbox is not in output text: {output}"
 
-    def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
-        action = ActionChains(self.driver)
-        action.drag_and_drop_by_offset(element, x_coords, y_coords)
-        action.perform()
 
-    def action_drag_and_drop_to_element(self, what, where):
-        action = ActionChains(self.driver)
-        action.drag_and_drop(what, where)
-        action.perform()
